efb72840f4c3b03ba52270b16628047747209e4f
Simon Ser <contact@emersion.fr>
2021-12-15 17:31:58 +0100

    Add file upload support

--- client.go
+++ client.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -21,8 +22,9 @@ func New(endpoint string, hc *http.Client) *Client {
 }
 
 type Operation struct {
-	query string
-	vars  map[string]interface{}
+	query   string
+	vars    map[string]interface{}
+	uploads map[string]Upload
 }
 
 func NewOperation(query string) *Operation {
@@ -32,8 +34,33 @@ func NewOperation(query string) *Operation {
 func (op *Operation) Var(k string, v interface{}) {
 	if op.vars == nil {
 		op.vars = make(map[string]interface{})
+		op.uploads = make(map[string]Upload)
+	}
+	if _, ok := op.vars[k]; ok {
+		panic(fmt.Sprintf("gqlclient: called Operation.Var twice on %q", k))
 	}
 	op.vars[k] = v
+
+	// TODO: support more deeply nested uploads
+	switch v := v.(type) {
+	case Upload:
+		op.uploads[k] = v
+	case *Upload:
+		if v != nil {
+			op.uploads[k] = *v
+		}
+	case []Upload:
+		for i, upload := range v {
+			upload := upload // copy
+			op.uploads[fmt.Sprintf("%v.%v", k, i)] = upload
+		}
+	case []*Upload:
+		for i, upload := range v {
+			if upload != nil {
+				op.uploads[fmt.Sprintf("%v.%v", k, i)] = *upload
+			}
+		}
+	}
 }
 
 func (c *Client) Execute(ctx context.Context, op *Operation, data interface{}) error {
@@ -50,11 +77,25 @@ func (c *Client) Execute(ctx context.Context, op *Operation, data interface{}) e
 		return fmt.Errorf("failed to encode request payload: %v", err)
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.endpoint, &reqBuf)
+	var reqBody io.Reader
+	var contentType string
+	if len(op.uploads) > 0 {
+		pr, pw := io.Pipe()
+		defer pr.Close()
+
+		reqBody = pr
+		contentType = writeMultipart(pw, op.uploads, &reqBuf)
+	} else {
+		reqBody = &reqBuf
+		contentType = "application/json; charset=utf-8"
+	}
+
+	// io.TeeReader(reqBody, os.Stderr)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.endpoint, reqBody)
 	if err != nil {
 		return fmt.Errorf("failed to create HTTP request: %v", err)
 	}
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.Header.Set("Content-Type", contentType)
 	req.Header.Set("Accept", "application/json")
 
 	resp, err := c.http.Do(req)
@@ -67,6 +108,7 @@ func (c *Client) Execute(ctx context.Context, op *Operation, data interface{}) e
 		Data   interface{}
 		Errors []Error
 	}{Data: data}
+	// io.TeeReader(resp.Body, os.Stderr)
 	if err := json.NewDecoder(resp.Body).Decode(&respData); err != nil {
 		return fmt.Errorf("failed to decode response payload: %v", err)
 	}
